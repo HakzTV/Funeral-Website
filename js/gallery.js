8f8408967f312b6d0d6c3dbe669cff7123b28c8a
Telvin Varfley <telvinvarfley@gmail.com>
2023-04-24 14:32:23 +0000

    Gallery  Implemented

---
+++ js/gallery.js
@@ -0,0 +1,54 @@
+
+
+const mainSection = document.querySelector('main')
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() >= mainSection.offsetTop) {
+      $(".nav").addClass("affix");
+    } else {
+      $(".nav").removeClass("affix");
+    }
+  });
+
+// Gallery individual view
+
+let darkBoxVisible = false;
+
+function showDarkbox(url) {
+  if(!darkBoxVisible) {
+   let x = (window.innerWidth - 1280) / 2;
+   let y = window.scrollY + 50;
+   // Create the darkBox
+   var div = document.createElement("div");
+   div.id = "darkbox";
+   div.innerHTML = '<img class="darkboximg" src="'+url+'" />';
+   document.body.appendChild(div);
+   let box = document.getElementById("darkbox");
+   box.style.left = x.toString()+"px";
+   box.style.top = y.toString()+"px";
+   box.style.height = 'auto';
+   box.addEventListener('click', (event) => {
+    // Remove it
+    let element = document.getElementById("darkbox");
+    element.parentNode.removeChild(element);
+    darkBoxVisible = false;
+   })
+   darkBoxVisible = true;
+ } else {
+   // Remove it
+   let element = document.getElementById("darkbox");
+   element.parentNode.removeChild(element);
+   darkBoxVisible = false;
+}
+}
+
+window.addEventListener('load', (event) => {
+  let images = document.querySelectorAll(".ga img");
+  if(images !== null && images !== undefined && images.length > 0) {
+   images.forEach(function(img) {
+    img.addEventListener('click', (evt) => {
+     showDarkbox(img.src);
+    });
+   });
+  }
+ });
